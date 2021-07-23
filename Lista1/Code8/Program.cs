60ba83f39c76b484e03b822fbdb8185000ca1034
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-07-23 19:17:43 -0300

    Add files via upload

---
+++ Lista1/Code8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Code8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, c;
+
+            Console.WriteLine("Digite a temperatura em Kelvin para convertermos para Celsius: ");
+            k = double.Parse(Console.ReadLine());
+            c = k - 273.15;
+
+            Console.WriteLine("A temperatura de Celsius é: " + c);
+        }
+    }
+}
