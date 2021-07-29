3adc5979bceef05042429d70e4280d743c0945d6
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-07-29 09:35:14 -0300

    Add files via upload

---
+++ Lista2/Code11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Code11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float trab, aval, exame, media;
+
+            Console.WriteLine("Digite a nota do trabalho de laboratório (0 a 10): ");
+            trab = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da avaliação semestral (0 a 10): ");
+            aval = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do exame final (0 a 10): ");
+            exame = float.Parse(Console.ReadLine());
+
+            media = ((trab * 2) + (aval * 3) + (exame * 5)) / 10;
+
+            Console.WriteLine("A média das notas que foram digitadas é " + media);
+
+            if ((media >= 5) && (media <= 10))
+                Console.WriteLine("Aluno aprovado!");
+
+            else if ((media >= 3) && (media <= 4.9))
+                Console.WriteLine("Aluno de recuperação.");
+
+            else if ((media >= 0) && (media <= 2.9))
+                Console.WriteLine("Aluno reprovado.");
+        }
+    }
+}
---
+++ Lista2/Code13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Code13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float alt, area, menor, maior;
+
+            Console.WriteLine("Digite a altura do trapézio: ");
+            alt = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a base menor do trapézio: ");
+            menor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a base maior do trapézio: ");
+            maior = float.Parse(Console.ReadLine());
+
+            area = ((menor + maior) * alt) / 2;
+            Console.WriteLine("A área do trapézio é: " + area);
+        }
+    }
+}
