e77885accb67f1a270fb707acf90d90237d70f34
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-07-29 09:31:33 -0300

    Add files via upload

---
+++ Lista2/Code5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Code5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num == num2)
+                Console.WriteLine("Os dois números que foram digitados são iguais!");
+            else if (num > num2)
+                Console.WriteLine("O número " + num + " é maior que o número " + num2);
+            else
+                Console.WriteLine("O número " + num2 + " é maior que o número " + num);
+        }
+    }
+}
---
+++ Lista2/Code6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Code6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+
+            Console.WriteLine("Digite a primeira nota: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            if ((n1 >= 0.0) && (n1 <= 10.0))
+            {
+                if ((n2 >= 0.0) && (n2 <= 10.0))
+                {
+                    Console.WriteLine("A média das notas que foram digitadas é: " + (n1 + n2) / 2);
+                }
+                else
+
+                    Console.WriteLine("A segunda nota é inválida! (digite um número entre 0.0 e 10.0) ");
+            }
+            else if (((n1 < 0.0) || (n1 > 10.0)) && ((n2 < 0.0) || (n2 > 10.0)))
+                Console.WriteLine("As duas notas são inválidas! (digite valores entre 0.0 e 10.0)");
+            else
+                Console.WriteLine("A primeira nota é inválida! (digite um número entre 0.0 e 10.0) ");
+        }
+    }
+}
