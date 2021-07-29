1c005facb28155a02207769b2c6f2e3eec201c8c
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-07-29 09:36:12 -0300

    Add files via upload

---
+++ Lista2/Code14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Code14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float lado1, lado2, lado3;
+
+            Console.WriteLine("Primeiro lado: ");
+            lado1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo lado: ");
+            lado2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Terceiro lado: ");
+            lado3 = float.Parse(Console.ReadLine());
+
+            if (((lado1 + lado2) > lado3) && ((lado1 + lado3) > lado2) && ((lado3 + lado2) > lado1))
+            {
+                if ((lado1 == lado2) && (lado2 == lado3))
+                    Console.WriteLine("O triângulo é equilátero!");
+                else if ((lado1 != lado2) && (lado1 != lado3) && (lado2 != lado3))
+                    Console.WriteLine("O triângulo é escaleno!");
+                else
+                    Console.WriteLine("O triângulo é isósceles!");
+            }
+            else
+                Console.WriteLine("Os valores digitados não podem ser valores dos lados de um triângulo!");
+        }
+    }
+}
---
+++ Lista2/Code15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Code15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trab, idade;
+
+            Console.WriteLine("Digite a sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite durante quantos anos você já trabalhou: ");
+            trab = int.Parse(Console.ReadLine());
+
+            if ((idade >= 65) || (trab >= 30) || ((idade >= 60) && (trab >= 25)))
+                Console.WriteLine("Você pode se aposentar!");
+            else
+                Console.WriteLine("Você não pode se aposentar");
+        }
+    }
+}
