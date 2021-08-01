2f52594acad6a5842f3b600120353efc8136981c
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-08-01 19:40:36 -0300

    Add files via upload

---
+++ Lista3/Code4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Code4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nOs " + num + " primeiros número naturais ímpares são: ");
+
+            for (int i = 0; num > 0; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    num--;
+                }
+            }
+        }
+    }
+}
