e24ef83b252a1a06cefcec5777d69bcb1e13ddab
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-08-01 19:42:52 -0300

    Add files via upload

---
+++ Lista3/Code7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Code7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite um número par positvo: ");
+            num = int.Parse(Console.ReadLine());
+
+            if ((num > 0) && (num % 2 == 0))
+            {
+                for (int i = 0; i <= num; ++i)
+                    if (i % 2 == 0)
+
+                        Console.Write(i + "\n");
+            }
+        }
+    }
+}
