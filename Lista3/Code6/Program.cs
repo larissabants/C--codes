20bbfbaacd94f75b747e77aa801123da65efc205
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-08-01 19:42:10 -0300

    Add files via upload

---
+++ Lista3/Code6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Code6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, maior, menor;
+
+            Console.WriteLine("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            maior = num;
+            menor = num;
+
+            if (num >= 0)
+                do
+                {
+                    num = int.Parse(Console.ReadLine());
+                    if (num > 0)
+                    {
+                        if (num > maior)
+                            maior = num;
+                        if (num < menor)
+                            menor = num;
+                    }
+                } while (!(num < 0));
+
+            Console.WriteLine("O maior número digitado é: " + maior);
+            Console.WriteLine("O menor número digitado é: " + menor);
+
+        }
+    }
+}
