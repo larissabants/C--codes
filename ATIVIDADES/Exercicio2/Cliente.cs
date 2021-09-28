908b68e165c3de40ae67113c39508f92e5aeef49
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-09-28 09:49:04 -0300

    Add files via upload

---
+++ ATIVIDADES/Exercicio2/Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Cliente
+    {
+        private string nome;
+        private string sobrenome;
+        private string cpf;
+        private Conta conta;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            this.sobrenome = sobrenome;
+        }
+
+        public string getCpf()
+        {
+            return this.cpf;
+        }
+        public void setCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public Conta getConta()
+        {
+            return this.conta;
+        }
+        public void setConta(Conta conta)
+        {
+            this.conta = conta;
+        }
+    }
+}
