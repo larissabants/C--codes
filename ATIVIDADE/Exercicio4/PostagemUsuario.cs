e6243be33b60a631170dc041a1af3886e99bd234
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-09-28 10:02:33 -0300

    Add files via upload

---
+++ ATIVIDADE/Exercicio4/PostagemUsuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class UsuarioPostagem
+    {
+        static void Main(string[] args)
+        {
+
+            Postagem primeirapost = new Postagem();
+            primeirapost.setTitulo("Postagem 1");
+            primeirapost.setData("24/08/2021");
+            primeirapost.setConteudo("Primeiro conteúdo");
+            primeirapost.setVisitas(1);
+
+            Postagem segundapost = new Postagem();
+            segundapost.setTitulo("Postagem 2");
+            segundapost.setData("24/08/2021");
+            segundapost.setConteudo("Segundo conteúdo");
+            segundapost.setVisitas(2);
+
+            Postagem terceirapost = new Postagem();
+            terceirapost.setTitulo("Exercício");
+            terceirapost.setData("24/08/2021");
+            terceirapost.setConteudo("Terceiro conteúdo");
+            terceirapost.setVisitas(3);
+
+            Postagem[] Postagens = new Postagem[3];
+            Postagens[0] = primeirapost;
+            Postagens[1] = segundapost;
+            Postagens[2] = terceirapost;
+
+            Usuario user = new Usuario();
+            user.setNome("Larissa");
+            user.setSobrenome("Barros");
+            user.setSenha("Abc1234");
+            for (int i = 0; i < 3; i++)
+            {
+                user.setPostagens(Postagens);
+            }
+
+            Console.WriteLine("Nome: " + user.getNome());
+            Console.WriteLine("Sobrenome: " + user.getSobrenome());
+            Console.WriteLine("Senha: " + user.getSenha() + "\n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Título: " + user.getPostagens()[i].getTitulo());
+                Console.WriteLine("Data: " + user.getPostagens()[i].getData());
+                Console.WriteLine("Conteúdo: " + user.getPostagens()[i].getConteudo());
+                Console.WriteLine("Visitas: " + user.getPostagens()[i].getVisitas() + "\n");
+            }
+        }
+    }
+}
---
+++ ATIVIDADE/Exercicio4/Usuario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Usuario
+    {
+        private string nome;
+        private string sobrenome;
+        private string senha;
+        private Postagem[] postagens = new Postagem[3];
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            this.sobrenome = sobrenome;
+        }
+
+        public string getSenha()
+        {
+            return this.senha;
+        }
+        public void setSenha(string senha)
+        {
+            this.senha = senha;
+        }
+
+        public Postagem[] getPostagens()
+        {
+            return this.postagens;
+        }
+        public void setPostagens(Postagem[] postagens)
+        {
+            this.postagens = postagens;
+        }
+    }
+}
