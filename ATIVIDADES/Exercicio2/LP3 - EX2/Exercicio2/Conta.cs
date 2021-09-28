db5e10e14fa885e89256aae825d91a85a4423019
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-09-28 09:54:57 -0300

    Add files via upload

---
+++ ATIVIDADES/Exercicio2/LP3 - EX2/Exercicio2/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Conta
+    {
+        private string agencia;
+        private int numero;
+        private float saldo;
+
+        public string getAgencia()
+        {
+            return this.agencia;
+        }
+        public void setAgencia(string agencia)
+        {
+            this.agencia = agencia;
+        }
+
+        public int getNumero()
+        {
+            return this.numero;
+        }
+        public void setNumero(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public float getSaldo()
+        {
+            return this.saldo;
+        }
+        public void setSaldo(float saldo)
+        {
+            this.saldo = saldo;
+        }
+    }
+}
