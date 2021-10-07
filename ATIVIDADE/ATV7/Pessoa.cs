a5aa77dd6590de2249b898ec0a7505eefbf6536b
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-10-07 20:22:05 -0300

    Add files via upload

---
+++ ATIVIDADE/ATV7/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A7
+{
+    class Pessoa
+    {
+        private string nome = "Larissa";
+        private string sobrenome = "Barros";
+        private byte idade = 18;
+        private float altura = 1.60F;
+        private float peso = 50.0F;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+    }
+}
---
+++ ATIVIDADE/ATV7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa aux = new Pessoa();
+            Console.WriteLine("Nome: " + aux.getNome());
+        }
+    }
+}
